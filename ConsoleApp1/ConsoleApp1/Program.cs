8d6f25b3602b29d48bd5ebab84d77936fb14f85b
herenuf <1019121647@qq.com>
2018-03-22 21:06:22 +0800

    consoleapp1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,14 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int INT = 123;
-            char CHAR = 'a';
-            string STRING = "我要自学网";
-            object OBJECT = 1024;
-            Console.WriteLine(INT);
-            Console.WriteLine(CHAR);
-            Console.WriteLine(STRING);
-            Console.WriteLine(OBJECT);
+            //int INT = 123;
+            //char CHAR = 'a';
+            //string STRING = "我要自学网";
+            //object OBJECT = 1024;
+            //Console.WriteLine(INT);
+            //Console.WriteLine(CHAR);
+            //Console.WriteLine(STRING);
+            //Console.WriteLine(OBJECT);
+            //Console.ReadKey();
+            int a = 10;
+            double b = 3.14;
+            string c = "我要自学网";
+            bool mybool = false;
+            mybool = a is int;
+            Console.WriteLine("a is int\t"+mybool);// \t跳到下一个制表符
             Console.ReadKey();
         }
     }
